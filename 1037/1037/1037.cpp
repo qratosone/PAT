198cc95b8b972471fc1f81101c43bb80cfb3740d
The space of Qratosone <qratosone@live.com>
2015-11-09 08:20:45 +0800

    Add 1037 unfinished

---
+++ 1037/1037/1037.cpp
@@ -0,0 +1,27 @@
+// 1037.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include   <iostream>   
+#include   <cstdlib>   
+//是赋值以逗号为分隔符  
+using   namespace   std;
+
+int   main()
+{
+	int   a, b, c,d,e,f;
+	cin >> a; cin.ignore(1, ', ');
+	cin >> b; cin.ignore(1, ', ');
+	cin >> c; cin.ignore(1, ', ');
+	cin >> d; cin.ignore(1, ', ');
+	cin >> e; cin.ignore(1, ', ');
+	cin >> f; cin.ignore(1, ', ');
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	cout << f << endl;
+	system("pause ");
+	return   0;
+}
