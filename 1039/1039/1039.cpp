390544deaf71d11e0f6919c9d93b46e7cde3c26c
The space of Qratosone <qratosone@live.com>
2015-11-15 00:28:22 +0800

    Add 1039

--- 1039/1039/1039.cpp
+++ 1039/1039/1039.cpp
@@ -8,8 +8,32 @@ using namespace std;
 
 int main()
 {
-	string round;
-	
+	string get;
+	string want;
+	cin >> get;
+	cin >> want;
+	int got = 0;
+	for (int i = 0; i <want.length(); i++)
+	{
+		for (int j = 0; j < get.length(); j++)
+		{
+			if (get[j]==want[i])
+			{
+				got++;
+				get[j] = '#';
+				break;
+			}
+		}
+	}
+	if (got==want.length())
+	{
+		cout << "Yes " << get.length() - want.length() << endl;
+	}
+	else
+	{
+		cout << "No " << want.length() - got << endl;
+	}
+
     return 0;
 }
 
