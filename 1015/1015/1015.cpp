88743e69bfa6ffdfdf343a75eff8a02158726121
The space of Qratosone <qratosone@live.com>
2015-12-04 00:25:54 +0800

    Done 1015

--- 1015/1015/1015.cpp
+++ 1015/1015/1015.cpp
@@ -10,10 +10,29 @@
 using namespace std;
 class student {
 public:
-	string ID;
+	int ID;
 	int moral;
 	int talent;
+	void output() {
+		printf("%d %d %d\n", ID, moral, talent);
+	}
 };
+bool cmp(student s1, student s2) {
+	int total1 = s1.moral + s1.talent;
+	int total2 = s2.moral + s2.talent;
+	if (total1!=total2)
+	{
+		return total1 > total2;
+	}
+	else
+	{
+		if (s1.moral!=s2.moral)
+		{
+			return s1.moral > s2.moral;
+		}
+		return s1.ID < s2.ID;
+	}
+}
 int main()
 {
 	int n, l, h;
@@ -24,11 +43,11 @@ int main()
 	vector<student>v4;
 	for (int i = 0; i < n; i++)
 	{
-		string t1;
+		int t1;
 		int  t2, t3;
 		student ts;
-		scanf_s("%s %d %d", t1, &t2, &t3);
-		ts.ID.assign(t1);
+		scanf_s("%d %d %d", &t1, &t2, &t3);
+		ts.ID=t1;
 		ts.moral = t2;
 		ts.talent = t3;
 		if (t2>=h&&t3>=h)
@@ -50,6 +69,28 @@ int main()
 				v4.push_back(ts);
 			}
 		}
+	}
+	int count = v1.size()+v2.size()+v3.size()+v4.size();
+	sort(v1.begin(), v1.end(), cmp);
+	sort(v2.begin(), v2.end(), cmp);
+	sort(v3.begin(), v3.end(), cmp);
+	sort(v4.begin(), v4.end(), cmp);
+	cout << count << endl;
+	for (int i = 0; i < v1.size(); i++)
+	{
+		v1[i].output();
+	}
+	for (int i = 0; i < v2.size(); i++)
+	{
+		v2[i].output();
+	}
+	for (int i = 0; i < v3.size(); i++)
+	{
+		v3[i].output();
+	}
+	for (int i = 0; i < v4.size(); i++)
+	{
+		v4[i].output();
 	}
     return 0;
 }
